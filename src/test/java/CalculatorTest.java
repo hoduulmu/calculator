68c95d305844b22140f0e1b1dc97bc11fa20ec84
hoduulmu <timewarp96@naver.com>
2024-02-08 10:41:03 +0900

    test: Calculator Add Test

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,21 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CalculatorTest {
+
+    @Test
+    void 계산기는_두_수를_더할_수_있다() {
+        // given
+        int a = 1;
+        int b = 2;
+        Calculator calculator = new Calculator();
+
+        // when
+        int actual = calculator.add(a, b);
+
+        // then
+        assertThat(actual).isEqualTo(3);
+    }
+}
