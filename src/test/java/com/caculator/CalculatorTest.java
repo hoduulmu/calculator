96c86576f1dbf7bc1319edf68494a5779f418933
hoduulmu <timewarp96@naver.com>
2024-02-08 12:45:22 +0900

    test: Calculator Calculate Test

--- src/test/java/com/caculator/CalculatorTest.java
+++ src/test/java/com/caculator/CalculatorTest.java
@@ -91,4 +91,18 @@ class CalculatorTest {
         // then
         assertThat(actual).isEqualTo(new BigDecimal("0.33333333333"));
     }
+
+    @Test
+    void 계산기는_입력된_연산_기호에_맞게_계산한다() {
+        // given
+        BigDecimal a = new BigDecimal("1");
+        BigDecimal b = new BigDecimal("2");
+        Calculator calculator = new Calculator();
+
+        // when
+        BigDecimal actual = calculator.calculate(a, b, "+");
+
+        // then
+        assertThat(actual).isEqualTo(new BigDecimal("3"));
+    }
 }
