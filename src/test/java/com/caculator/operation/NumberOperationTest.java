32608101acaf04d8e508ba2efe7e62b5444f4b48
hoduulmu <timewarp96@naver.com>
2024-02-08 13:33:13 +0900

    test: NumberOperation Fail Test For Input Is Not Number

---
+++ src/test/java/com/caculator/operation/NumberOperationTest.java
@@ -0,0 +1,24 @@
+package com.caculator.operation;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NumberOperationTest {
+
+    @Test
+    void 숫자_연산에_숫자가_아닌_문자를_입력하면_예외를_던진다() {
+        // given
+        NumberOperation numberOperation = operand -> null;
+        List<String> notNumber = List.of("notNumber");
+
+        // when
+        // then
+        assertThatThrownBy(() -> numberOperation.calculate(notNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("숫자만 입력해주세요.");
+    }
+}
