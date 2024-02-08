264b295a4fc1f97b59ffce652a152e9b7adb72fa
hoduulmu <timewarp96@naver.com>
2024-02-08 10:51:30 +0900

    test: Real Number Add Test

--- src/test/java/com/caculator/CalculatorTest.java
+++ src/test/java/com/caculator/CalculatorTest.java
@@ -2,6 +2,8 @@ package com.caculator;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CalculatorTest {
@@ -20,4 +22,17 @@ class CalculatorTest {
         assertThat(actual).isEqualTo(3);
     }
 
+    @Test
+    void 계산기는_두_실수를_더할_수_있다() {
+        // given
+        BigDecimal a = new BigDecimal("1.312321353131239");
+        BigDecimal b = new BigDecimal("2.312321353131239");
+        Calculator calculator = new Calculator();
+
+        // when
+        BigDecimal actual = calculator.add(a, b);
+
+        // then
+        assertThat(actual).isEqualTo(new BigDecimal("3.624642706262478"));
+    }
 }
