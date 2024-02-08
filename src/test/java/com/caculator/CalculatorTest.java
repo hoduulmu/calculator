399fc32c74b3a0c12c25ba3c36da8dbb539d6834
hoduulmu <timewarp96@naver.com>
2024-02-08 13:25:47 +0900

    test: Add Exception Test Of Not Supported Operation

--- src/test/java/com/caculator/CalculatorTest.java
+++ src/test/java/com/caculator/CalculatorTest.java
@@ -1,19 +1,19 @@
 package com.caculator;
 
-import com.caculator.operation.impl.Divide;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CalculatorTest {
 
     @Test
     void 계산기는_입력된_연산_기호에_맞게_계산한다() {
         // given
-        BigDecimal a = new BigDecimal("1");
-        BigDecimal b = new BigDecimal("2");
+        String a = "1";
+        String b = "2";
         Calculator calculator = new Calculator();
 
         // when
@@ -21,5 +21,19 @@ class CalculatorTest {
 
         // then
         assertThat(actual).isEqualTo(new BigDecimal("3"));
+    }
+
+    @Test
+    void 계산기는_지원하지_않는_연산_기호를_입력하면_예외를_던진다() {
+        // given
+        String a = "1";
+        String b = "2";
+        Calculator calculator = new Calculator();
+
+        // when
+        // then
+        assertThatThrownBy(() -> calculator.calculate(a, b, "x"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("지원하지 않는 연산자입니다.");
     }
 }
