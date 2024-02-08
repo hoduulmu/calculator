8c1c0fdbb6410ae3c534bf9ff3e85264b828fc59
hoduulmu <timewarp96@naver.com>
2024-02-08 13:39:24 +0900

    test: BinaryNumberOperation Fail Test If Number Size Not 2

---
+++ src/test/java/com/caculator/operation/BinaryNumberOperationTest.java
@@ -0,0 +1,23 @@
+package com.caculator.operation;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BinaryNumberOperationTest {
+
+    @Test
+    void 이항_연산에_2개_이상의_숫자를_입력하면_예외를_던진다() {
+        // given
+        BinaryNumberOperation binaryNumberOperation = (operand, secondOperand) -> null;
+        List<String> threeNumbers = List.of("1", "2", "3");
+        
+        // when
+        // then
+        assertThatThrownBy(() -> binaryNumberOperation.calculate(threeNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("2개의 숫자만 입력해주세요.");
+    }
+}
